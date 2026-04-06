3a6324b1d0e070f9ddd35d7e216b6d46bf2262eb
OH MAY ISHAR <Wadi3586@gmail.com>
2026-04-05 21:04:22 -0500

    Update test_solitaire.py

--- test_solitaire.py
+++ test_solitaire.py
@@ -1,10 +1,11 @@
+
 import unittest
-from solitaire_game import SolitaireGame
+from solitaire_game import EnglishSolitaire
 
 class TestSolitaireGame(unittest.TestCase):
 
     def setUp(self):
-        self.game = SolitaireGame()
+        self.game = EnglishSolitaire()
 
     def test_valid_move(self):
         result = self.game.make_move(3, 1, 3, 3)
@@ -16,6 +17,17 @@ class TestSolitaireGame(unittest.TestCase):
     def test_invalid_move(self):
         result = self.game.make_move(0, 2, 0, 4)
         self.assertFalse(result)
+
+    
+    #sprint 3: New test
+    def test_game_over(self):
+        game = EnglishSolitaire()
+        game.board = [[" " for _ in range(7)] for _ in range(7)]
+        self.assertTrue(game.is_game_over())
+
+    def test_count_pegs(self):
+        game = EnglishSolitaire()
+        self.assertEqual(game.count_pegs(), 32)
 
 if __name__ == "__main__":
     unittest.main()
